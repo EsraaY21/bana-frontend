06c5b5eac9dcf7dab4278ad498029f110b71ebdb
Esraa <esraayaareb@gmail.com>
2022-01-16 12:15:20 +0300

    add dummy data

---
+++ frontend/src/productsDummy.jsx
@@ -0,0 +1,61 @@
+const productsDummyData = [
+  {
+    id: 1,
+    name: 'Natural Day Cream',
+    image:
+      'https://image.freepik.com/free-psd/cosmetic-product-packaging-mockup_1150-40284.jpg',
+    price: 30,
+    brand: 'Natural',
+    category: 'Skin',
+    countInStock: 10,
+    short_description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut vestibulum lorem. Morbi porttitor feugiat',
+    long_description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum tellus. Curabitur tempor quis eros tempus lacinia. Nam bibendum pellentesque quam a convallis. Sed ut vulputate nisi. ',
+  },
+
+  {
+    id: 2,
+    name: 'Natural Day Cream',
+    image:
+      'https://image.freepik.com/free-psd/cosmetic-product-packaging-mockup_1150-40284.jpg',
+    price: 30,
+    brand: 'Natural',
+    category: 'Skin',
+    countInStock: 10,
+    short_description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut vestibulum lorem. Morbi porttitor feugiat',
+    long_description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum tellus. Curabitur tempor quis eros tempus lacinia. Nam bibendum pellentesque quam a convallis. Sed ut vulputate nisi. ',
+  },
+
+  {
+    id: 3,
+    name: 'Natural Day Cream',
+    image:
+      'https://images.pexels.com/photos/6663469/pexels-photo-6663469.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    price: 30,
+    brand: 'Natural',
+    category: 'Skin',
+    countInStock: 10,
+    short_description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut vestibulum lorem. Morbi porttitor feugiat',
+    long_description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum tellus. Curabitur tempor quis eros tempus lacinia. Nam bibendum pellentesque quam a convallis. Sed ut vulputate nisi. ',
+  },
+
+  {
+    id: 4,
+    name: 'Second Day Cream',
+    image:
+      'https://images.pexels.com/photos/6417953/pexels-photo-6417953.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    price: 30,
+    brand: 'CeraVe',
+    category: 'Skin',
+    countInStock: 10,
+    short_description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut vestibulum lorem. Morbi porttitor feugiat',
+    long_description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum tellus. Curabitur tempor quis eros tempus lacinia. Nam bibendum pellentesque quam a convallis. Sed ut vulputate nisi. ',
+  },
+];
