dc75e20d7d63558b6e4b1f73d07eff10d3c73e02
Esraa <esraayaareb@gmail.com>
2022-02-08 23:49:53 +0300

    add products endpoint

--- backend/ecommerce/models.py
+++ backend/ecommerce/models.py
@@ -6,8 +6,8 @@ class Entity(models.Model):
         abstract = True
 
     id = models.AutoField(primary_key=True, editable=False)
-    created = models.DateTimeField(editable=False, auto_now_add=True)
-    updated = models.DateTimeField(editable=False, auto_now=True)
+    dateCreated = models.DateTimeField(editable=False, auto_now_add=True)
+    dataUpdated = models.DateTimeField(editable=False, auto_now=True)
 
 
 class Product(Entity):
@@ -15,10 +15,27 @@ class Product(Entity):
     # images =
     price = models.DecimalField('price', max_digits=10, decimal_places=2)
     # category =
-    countInStock = models.IntegerField(default=0)
+    countInStock = models.IntegerField('Count In Stock', default=0)
     short_description = models.TextField(
-        'description', null=True, blank=True, max_length=500)
-    long_description = models.TextField('description', null=True, blank=True)
+        'Short Description', null=True, blank=True, max_length=500)
+    long_description = models.TextField(
+        'Long Description', null=True, blank=True)
 
     def __str__(self):
         return self.name
+
+
+# class Order(Entity):
+#     isDeliverd: models.BooleanField(default=False)
+
+#     def __str__(self):
+#         return str(self.id)
+
+
+# class OrderItem(Entity):
+#     product = models.ForeignKey(Product,
+#                                 on_delete=models.CASCADE)
+#     quantity = models.IntegerField('Quantity')
+
+#     def __str__(self):
+#         return self.product.name
