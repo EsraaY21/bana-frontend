f79d944a50fbeacfa1c13a1129e355ac747a3a36
Esraa <esraayaareb@gmail.com>
2022-02-08 22:35:47 +0300

    add rest framework

---
+++ backend/ecommerce/views.py
@@ -0,0 +1,13 @@
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+def getRoutes(request):
+    routes = ['/api/products/']
+    return JsonResponse(routes, safe=False)
+
+
+@api_view(['GET'])
+def getProducts(request):
+    return Response({"message": "Hello, world!"})
