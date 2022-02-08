c1489e40e05d188de0b5ec4db49b0256ed6d9924
Zahraa <sahrayarub@yahoo.de>
2022-02-08 11:19:43 +0300

    (card): fix style

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,136 +1,138 @@
-import { useState } from 'react';
-import { NavLink, useNavigate, Link } from 'react-router-dom';
-import { FaShoppingBag, FaSearch } from 'react-icons/fa';
-import logo from '../images/bana-care-logo.svg';
-import LocationBar from './LocationBar';
-import { useSelector } from 'react-redux';
-import { Popover, OverlayTrigger } from 'react-bootstrap';
+import { useState } from "react";
+import { NavLink, useNavigate, Link } from "react-router-dom";
+import { FaShoppingBasket, FaSearch } from "react-icons/fa";
+import logo from "../images/bana-care-logo.svg";
+import LocationBar from "./LocationBar";
+import { useSelector } from "react-redux";
+import { Popover, OverlayTrigger } from "react-bootstrap";
 
 const Header = () => {
-  const products = useSelector((state) => state.products.value);
-  const [searchValue, setSearchValue] = useState('');
-  const navigate = useNavigate();
-  const cartItems = useSelector((state) => state.cart.value);
+	const products = useSelector((state) => state.products.value);
+	const [searchValue, setSearchValue] = useState("");
+	const navigate = useNavigate();
+	const cartItems = useSelector((state) => state.cart.value);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    navigate({
-      pathname: `/shop/${searchValue}`,
-    });
-    setSearchValue('');
-  };
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		navigate({
+			pathname: `/shop/${searchValue}`,
+		});
+		setSearchValue("");
+	};
 
-  return (
-    <>
-      <nav className="navbar navbar-expand-lg">
-        <div className="container-fluid px-5 py-3">
-          {/* logo */}
-          <NavLink className="navbar-brand" to="/">
-            <img src={logo} alt="logo" width="75" className="img-fluid" />
-          </NavLink>
+	return (
+		<>
+			<nav className="navbar navbar-expand-lg">
+				<div className="container-fluid px-5 py-3">
+					{/* logo */}
+					<NavLink className="navbar-brand" to="/">
+						<img src={logo} alt="logo" width="75" className="img-fluid" />
+					</NavLink>
 
-          <button
-            className="navbar-toggler color-blue-dark navbar-light"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div
-            className="collapse navbar-collapse ms-auto"
-            id="navbarSupportedContent"
-          >
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
-              {['Home', 'Shop', 'Services', 'About', 'Contact'].map(
-                (pageName, index) => (
-                  <li className="nav-item pb-0" key={index}>
-                    <NavLink
-                      className={(navData) =>
-                        navData.isActive
-                          ? 'nav-link color-blue-dark active-link'
-                          : 'nav-link color-blue-dark'
-                      }
-                      to={
-                        pageName === 'Home' ? '/' : `/${pageName.toLowerCase()}`
-                      }
-                    >
-                      {pageName}
-                    </NavLink>
-                  </li>
-                )
-              )}
-            </ul>
-            <div className="d-flex align-items-center">
-              {/* Search */}
+					<button
+						className="navbar-toggler color-blue-dark navbar-light"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target="#navbarSupportedContent"
+						aria-controls="navbarSupportedContent"
+						aria-expanded="false"
+						aria-label="Toggle navigation"
+					>
+						<span className="navbar-toggler-icon"></span>
+					</button>
+					<div
+						className="collapse navbar-collapse ms-auto"
+						id="navbarSupportedContent"
+					>
+						<ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
+							{["Home", "Shop", "Services", "About", "Contact"].map(
+								(pageName, index) => (
+									<li className="nav-item pb-0" key={index}>
+										<NavLink
+											className={(navData) =>
+												navData.isActive
+													? "nav-link color-blue-dark active-link"
+													: "nav-link color-blue-dark"
+											}
+											to={
+												pageName === "Home" ? "/" : `/${pageName.toLowerCase()}`
+											}
+										>
+											{pageName}
+										</NavLink>
+									</li>
+								)
+							)}
+						</ul>
+						<div className="d-flex align-items-center">
+							{/* Search */}
 
-              <OverlayTrigger
-                trigger="click"
-                key="bottom"
-                placement="bottom"
-                overlay={
-                  <Popover id="popover-positioned-bottom">
-                    <Popover.Header as="h3">Suggestions</Popover.Header>
-                    <Popover.Body>
-                      {products
-                        .filter((product) =>
-                          product.name
-                            .toLowerCase()
-                            .includes(
-                              searchValue ? searchValue.toLowerCase() : ''
-                            )
-                        )
-                        .map((product) => (
-                          <Link to={`/products/${product.id}`}>
-                            <p className="mb-3">{product.name}</p>
-                          </Link>
-                        ))}
-                      <Link to={`/shop/${searchValue}`}>
-                        <strong>View all results</strong>
-                      </Link>
-                    </Popover.Body>
-                  </Popover>
-                }
-              >
-                <form onSubmit={handleSubmit}>
-                  <div className="d-flex align-items-center" id="navbar_search">
-                    <FaSearch className="navbar_search_icon" />
-                    <input
-                      value={searchValue}
-                      onChange={(e) => {
-                        setSearchValue(e.target.value);
-                      }}
-                      type="text"
-                      className="form-control border-0 rounded-4 "
-                      placeholder="Search Products ..."
-                    />
-                  </div>
-                </form>
-              </OverlayTrigger>
+							<OverlayTrigger
+								trigger="click"
+								key="bottom"
+								placement="bottom"
+								overlay={
+									<Popover id="popover-positioned-bottom">
+										<Popover.Header as="h3">Suggestions</Popover.Header>
+										<Popover.Body>
+											{products
+												.filter((product) =>
+													product.name
+														.toLowerCase()
+														.includes(
+															searchValue ? searchValue.toLowerCase() : ""
+														)
+												)
+												.map((product) => (
+													<Link to={`/products/${product.id}`}>
+														<p className="mb-3">{product.name}</p>
+													</Link>
+												))}
+											<Link to={`/shop/${searchValue}`}>
+												<strong>View all results</strong>
+											</Link>
+										</Popover.Body>
+									</Popover>
+								}
+							>
+								<form onSubmit={handleSubmit}>
+									<div className="d-flex align-items-center" id="navbar_search">
+										<FaSearch className="navbar_search_icon" />
+										<input
+											value={searchValue}
+											onChange={(e) => {
+												setSearchValue(e.target.value);
+											}}
+											type="text"
+											className="form-control border-0 rounded-4 "
+											placeholder="Search Products ..."
+										/>
+									</div>
+								</form>
+							</OverlayTrigger>
 
-              <button className="btn">
-                <NavLink to="/cart">
-                  <div className="badge-container">
-                    <FaShoppingBag className="fs-5" />
-
-                    <span className="badge-count badge">
-                      {cartItems
-                        .map((item) => item.quantity)
-                        .reduce((prev, curr) => prev + curr, 0)}
-                    </span>
-                  </div>
-                </NavLink>
-              </button>
-            </div>
-          </div>
-        </div>
-      </nav>
-      <LocationBar />
-    </>
-  );
+							<button className="btn">
+								<NavLink to="/cart">
+									<div className="badge-container">
+										<button type="button" class="btn btn-sm position-relative">
+											<FaShoppingBasket className="fs-5 position-relative" />
+											<span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-brown">
+												{cartItems
+													.map((item) => item.quantity)
+													.reduce((prev, curr) => prev + curr, 0)}
+												<span class="visually-hidden">items in cart</span>
+											</span>
+										</button>
+									</div>
+								</NavLink>
+							</button>
+						</div>
+					</div>
+				</div>
+			</nav>
+			<LocationBar />
+		</>
+	);
 };
 
 export default Header;
--- frontend/src/screens/Cart.jsx
+++ frontend/src/screens/Cart.jsx
@@ -1,130 +1,133 @@
-import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { FaTrash } from 'react-icons/fa';
-import { removeFromCart, changeQuantityByOne } from '../features/cartSlice';
-import { Table, Button, CloseButton as DeleteIcon } from 'react-bootstrap';
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { FaTrash } from "react-icons/fa";
+import { removeFromCart, changeQuantityByOne } from "../features/cartSlice";
+import { Table, Button, CloseButton as DeleteIcon } from "react-bootstrap";
 
 const Cart = () => {
-  const cartItems = useSelector((state) => state.cart.value);
-  const dispatch = useDispatch();
+	const cartItems = useSelector((state) => state.cart.value);
+	const dispatch = useDispatch();
 
-  const handleDelete = (cartItem) => {
-    dispatch(removeFromCart(cartItem));
-  };
+	const handleDelete = (cartItem) => {
+		dispatch(removeFromCart(cartItem));
+	};
 
-  const changeQuantityByOneHandler = (
-    operation,
-    productId,
-    currentQuantity
-  ) => {
-    dispatch(
-      changeQuantityByOne({
-        operation: operation,
-        productId: productId,
-        currentQuantity: currentQuantity,
-      })
-    );
-  };
+	const changeQuantityByOneHandler = (
+		operation,
+		productId,
+		currentQuantity
+	) => {
+		dispatch(
+			changeQuantityByOne({
+				operation: operation,
+				productId: productId,
+				currentQuantity: currentQuantity,
+			})
+		);
+	};
 
-  return (
-    <div className="cart container text-center">
-      <h1 className="my-5">Cart</h1>
-      <main className="row g-5">
-        <div className="col-md-5 col-lg-3 order-md-last">
-          <div className="card rounded-4 text-start p-4">
-            <h4>Cart Totals</h4>
-            <hr className="my-0" />
-            <p>
-              Total
-              <span>
-                {' '}
-                {cartItems
-                  .map((item) => item)
-                  .reduce((prev, curr) => prev + curr.quantity * curr.price, 0)}
-                $
-              </span>
-            </p>
-            <hr className="my-0" />
-            <span className="color-gray-light">
-              Shipping calculated at Checkout
-            </span>
-            <button className="bg-blue-dark rounded-4">
-              <Link to="/checkout">Proceed to Checkout</Link>
-            </button>
-          </div>
-        </div>
+	return (
+		<div className="cart container text-center pb-5">
+			<h1 className="mt-5 mb-2">Cart</h1>
+			<main className="row g-5">
+				<div className="col-md-5 col-lg-3 order-md-last">
+					<div className="card rounded-4 text-start p-3">
+						<h4>Cart Totals</h4>
+						<hr className="my-0" style={{ color: "#8f8989" }} />
+						<div className="d-flex justify-content-between mt-3">
+							<p className="mt-3x">Total</p>
+							<span className="color-brown fw-bold">
+								{" "}
+								{cartItems
+									.map((item) => item)
+									.reduce((prev, curr) => prev + curr.quantity * curr.price, 0)}
+								$
+							</span>
+						</div>
 
-        {/* TABLE */}
-        <div className="col-md-7 col-lg-9">
-          <div className="card">
-            <Table responsive>
-              <thead>
-                <tr>
-                  {/* {['Product', 'Quantity', 'Price', 'SubTotal', ''].map(
+						<hr className="my-0" style={{ color: "#8f8989" }} />
+						<span className="color-gray-dark mt-3 mb-3">
+							Shipping calculated at Checkout
+						</span>
+						<button className="btn btn-blue-dark">
+							<Link to="/checkout">Proceed to Checkout</Link>
+						</button>
+					</div>
+				</div>
+
+				{/* TABLE */}
+				<div className="col-md-7 col-lg-9">
+					<div className="card">
+						<Table responsive>
+							<thead className="table-light">
+								<tr>
+									{/* {['Product', 'Quantity', 'Price', 'SubTotal', ''].map(
                     (title, index) => (
                       <th key={index} style={{ width: '40%' }}>
                         {title}
                       </th>
                     )
                   )} */}
-                  <th style={{ width: '61%' }}>Product</th>
-                  <th style={{ width: '11%' }}>Quantity</th>
-                  <th style={{ width: '11%' }}>Price</th>
-                  <th style={{ width: '11%' }}>SubTotal</th>
-                  <th style={{ width: '6%' }}></th>
-                </tr>
-              </thead>
-              <tbody>
-                {cartItems.map((cartItem) => (
-                  <tr>
-                    <td>
-                      <Link to={`/products/${cartItem.id}`}>
-                        <div className="row">
-                          <div className="col-lg-3 ">
-                            <img src={cartItem.images[1]} alt={cartItem.name} />
-                          </div>
-                          <div className="col-lg-9 text-start d-flex align-items-center">
-                            <span>{cartItem.name}</span>
-                          </div>
-                        </div>
-                      </Link>
-                    </td>
+									<th style={{ width: "61%" }}>Product</th>
+									<th style={{ width: "11%" }}>Price</th>
+									<th style={{ width: "11%" }}>Quantity</th>
+									<th style={{ width: "11%" }}>SubTotal</th>
+									<th style={{ width: "6%" }}></th>
+								</tr>
+							</thead>
+							<tbody>
+								{cartItems.map((cartItem) => (
+									<tr>
+										<td>
+											<Link to={`/products/${cartItem.id}`}>
+												<div className="row">
+													<div className="col-lg-3 ">
+														<img src={cartItem.images[1]} alt={cartItem.name} />
+													</div>
+													<div className="col-lg-9 text-start d-flex align-items-center">
+														<span>{cartItem.name}</span>
+													</div>
+												</div>
+											</Link>
+										</td>
 
-                    <td>{cartItem.price}$</td>
+										<td className="align-middle">{cartItem.price}$</td>
 
-                    <td>
-                      <div className="d-flex align-items-center">
-                        <Button
-                          variant="light"
-                          onClick={() =>
-                            changeQuantityByOneHandler(
-                              'add',
-                              cartItem.id,
-                              cartItem.quantity
-                            )
-                          }
-                        >
-                          +
-                        </Button>
-                        <span className="mx-2">{cartItem.quantity}</span>
-                        <Button
-                          variant="light"
-                          onClick={() =>
-                            changeQuantityByOneHandler(
-                              'subtract',
-                              cartItem.id,
-                              cartItem.quantity
-                            )
-                          }
-                        >
-                          -
-                        </Button>
-                      </div>
-                    </td>
+										<td className="align-middle">
+											<div className="d-flex align-items-center">
+												<Button
+													variant="light"
+													onClick={() =>
+														changeQuantityByOneHandler(
+															"add",
+															cartItem.id,
+															cartItem.quantity
+														)
+													}
+												>
+													+
+												</Button>
+												<span className="mx-2">{cartItem.quantity}</span>
+												<Button
+													variant="light"
+													onClick={() =>
+														changeQuantityByOneHandler(
+															"subtract",
+															cartItem.id,
+															cartItem.quantity
+														)
+													}
+												>
+													-
+												</Button>
+											</div>
+										</td>
 
-                    <td>{cartItem.price * cartItem.quantity}$</td>
-                    <td>
-                      {/* <FaTrash
+										<td className="align-middle">
+											{cartItem.price * cartItem.quantity}$
+										</td>
+										<td className="align-middle">
+											{/* <FaTrash
                           style={{
                             cursor: 'pointer',
                           }}
@@ -132,21 +135,21 @@ const Cart = () => {
                             handleDelete(cartItem);
                           }}
                         /> */}
-                      <DeleteIcon
-                        onClick={() => {
-                          handleDelete(cartItem);
-                        }}
-                      />
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </Table>
-          </div>
-        </div>
-      </main>
-    </div>
-  );
+											<DeleteIcon
+												onClick={() => {
+													handleDelete(cartItem);
+												}}
+											/>
+										</td>
+									</tr>
+								))}
+							</tbody>
+						</Table>
+					</div>
+				</div>
+			</main>
+		</div>
+	);
 };
 
 export default Cart;
