62d310f5622a579660e5c1988dba0585d503f53e
Esraa <esraayaareb@gmail.com>
2022-02-21 21:08:06 +0300

    improve lightbox and button hover effect

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,20 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import { store } from '../src/app/store';
 import { Provider } from 'react-redux';
+import SimpleReactLightbox from 'simple-react-lightbox';
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <Routes>
-        <Route path="/*" element={<App />} />
+        <Route
+          path="/*"
+          element={
+            <SimpleReactLightbox>
+              <App />
+            </SimpleReactLightbox>
+          }
+        />
       </Routes>
     </BrowserRouter>
   </Provider>,
